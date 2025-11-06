f9c941510cf0cd24de732bfa646761901abc76c0
Ruslan <rusrasbeer2331@gmail.com>
2025-11-06 18:35:09 +0200

    Переробила лаборатору 1

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './layout.html',
   styleUrls: ['./layout.css']
 })
-export class Layout {
-  appTitle = 'Автомобілі';
-}
+export class Layout {}
