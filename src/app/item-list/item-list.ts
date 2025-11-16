9a67f4dd52b44dbc1c890200762db0a35d7f7a0b
Ruslan <rusrasbeer2331@gmail.com>
2025-11-16 22:28:49 +0200

    Виконала лабораторну №5

--- src/app/item-list/item-list.ts
+++ src/app/item-list/item-list.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ItemCard } from '../item-card/item-card';
 import { Car } from '../shared/models/car.model';
+import { DataService } from '../shared/services/data.service';
 
 @Component({
   selector: 'app-items-list',
@@ -11,35 +12,16 @@ import { Car } from '../shared/models/car.model';
   templateUrl: './item-list.html',
   styleUrls: ['./item-list.css']
 })
-export class ItemsList {
-  search: string = '';
+export class ItemsList implements OnInit {
 
-  cars: Car[] = [
-    {
-      id: 1,
-      brand: 'BMW',
-      model: 'X5',
-      year: 2020,
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRavqSsrQGhMfJdtPXzxCw5ChDe35amcmv4Dg&s',
-      price: '$45,000'
-    },
-    {
-      id: 2,
-      brand: 'Audi',
-      model: 'A4',
-      year: 2021,
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxTIh1hdlJT2ea48_sOUTQAWAQbhTuQHPO9g&s',
-      price: '$39,000'
-    },
-    {
-      id: 3,
-      brand: 'Tesla',
-      model: 'Model 3',
-      year: 2023,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/9/91/2019_Tesla_Model_3_Performance_AWD_Front.jpg',
-      price: '$49,000'
-    }
-  ];
+  search: string = '';
+  cars: Car[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.cars = this.dataService.getItems();
+  }
 
   get filteredCars(): Car[] {
     return this.cars.filter(car =>
---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Car } from '../models/car.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private cars: Car[] = [
+    {
+      id: 1,
+      brand: 'BMW',
+      model: 'X5',
+      year: 2020,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRavqSsrQGhMfJdtPXzxCw5ChDe35amcmv4Dg&s',
+      price: '$45,000'
+    },
+    {
+      id: 2,
+      brand: 'Audi',
+      model: 'A4',
+      year: 2021,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxTIh1hdlJT2ea48_sOUTQAWAQbhTuQHPO9g&s',
+      price: '$39,000'
+    },
+    {
+      id: 3,
+      brand: 'Tesla',
+      model: 'Model 3',
+      year: 2023,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/9/91/2019_Tesla_Model_3_Performance_AWD_Front.jpg',
+      price: '$49,000'
+    }
+  ];
+
+  getItems(): Car[] {
+    return this.cars;
+  }
+}
