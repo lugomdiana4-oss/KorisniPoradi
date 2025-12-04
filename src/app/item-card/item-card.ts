7fa5dcdcedc96671d707ef7bfdb6d82337403ff9
Ruslan <rusrasbeer2331@gmail.com>
2025-12-04 12:20:27 +0200

    3 Лабораторна

--- src/app/item-card/item-card.ts
+++ src/app/item-card/item-card.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Advice } from '../shared/models/advice.model';
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './item-card.html',
   styleUrls: ['./item-card.css']
 })
