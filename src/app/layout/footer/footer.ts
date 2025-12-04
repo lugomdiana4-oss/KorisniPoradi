5e951005de14f9d8ecd0e7a2df0dae598b531823
Ruslan <rusrasbeer2331@gmail.com>
2025-12-04 11:40:16 +0200

    1 Лабораторна

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -7,3 +7,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.css']
 })
 export class Footer {}
+1
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -9,3 +9,4 @@ import { Component } from '@angular/core';
 export class Header {
   title = 'Корисні поради для повсякденного життя';
 }
+1
